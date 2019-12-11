227f4b194dfae2b2e2e0eee940c8b1229aac6b1b
IRINA <teamasterln@gmail.com>
2019-12-11 21:51:42 +0200

    update

--- src/DataStructureLink.java
+++ src/DataStructureLink.java
@@ -1,6 +1,19 @@
 public class DataStructureLink<T> {
-   public DataStructureLink(){
-       DataStructureConnector<T> dt1;
-       DataStructureConnector<T> dt2;
+    public DataStructureLink(DataStructureConnector<T> d1, DataStructureConnector<T> d2) {
+
+    }
+
+//  moveNextFromAToB () перемещает следующий элемент из первого
+//  конектора во второй конектор
+//  удалите, а затем добавьте). Если в первом соединителе больше нет элемента,
+//  метод возвращает значение false,  и ничего не происходит, в противном случае
+//  элементчы передается и возвращается значение true.
+    public boolean moveNextFromAToB(){
+    //
+        //  return ;
+    }
+
+    public void moveAllFromAToB(){
+
     }
     }

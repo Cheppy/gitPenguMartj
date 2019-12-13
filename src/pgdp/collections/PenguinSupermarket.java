c6f8bc186c998074cce30a871d69517ed602cc54
Cheppy <teamaster@gmail.com>
2019-12-13 21:01:27 +0200

    update

--- src/pgdp/collections/PenguinSupermarket.java
+++ src/pgdp/collections/PenguinSupermarket.java
@@ -15,9 +15,39 @@ public class PenguinSupermarket {
    // Checkout getCheckoutWithSmallestQueue (){
   //  return Checkout null;}
 
-    void CloseCheckout (int index){
+    public static Checkout[] removeTheElement(Checkout[] arr,
+                                         int index)
+    {
+        if (arr == null
+                || index < 0
+                || index >= arr.length) {
 
+            return arr;
+        }
+
+
+        Checkout[] anotherArray = new Checkout[arr.length - 1];
+
+        for (int i = 0, k = 0; i < arr.length; i++) {
+
+
+            if (i == index) {
+                continue;
+            }
+
+
+            anotherArray[k++] = arr[i];
+        }
+
+        // return the resultant array
+        return anotherArray;
     }
+
+    void CloseCheckout (int index){
+        removeTheElement(checkouts, index);
+        }
+
+
 
     void  serveCustomers (){
         serveNextCustomer ();

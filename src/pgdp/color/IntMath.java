a93decf5e8d16d26954295493304843373772d94
IRINA <teamasterln@gmail.com>
2019-12-18 11:34:28 +0200

    a

---
+++ src/pgdp/color/IntMath.java
@@ -0,0 +1,19 @@
+package pgdp.color;
+/**
+ * Vorgegebene Klasse, hier drin wird nichts bewertet !!!
+ * &lt;p&gt;
+ * Bitte nicht verändern !!
+ */
+public class IntMath {
+    private IntMath() {
+    }
+    public static int powerOfTwo(int exponent) {
+        if (exponent > 31)
+        ExceptionUtil.unsupportedOperation("exponent too large:" + exponent);
+        if (exponent < 0)
+        ExceptionUtil.unsupportedOperation("exponent cannot be negative: " +
+                exponent);
+        return 1 << exponent;
+
+    }
+}
